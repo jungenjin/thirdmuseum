9038f128ff7a6db9b21263f77345bbe7fc670234
bye0607 <bye0607@gmail.com>
2023-02-24 15:05:09 +0900

    kej, SubVisual 컴포넌트 반응형 작업 완료

--- tm-front/src/common/SubVisual.jsx
+++ tm-front/src/common/SubVisual.jsx
@@ -5,12 +5,12 @@ function SubVisual(props) {
     const src = `/resource/common/${props.imgName}_visual.png`;
     return (
         <>
-        <div className="subvisual text-center pb-5">
-            <h1 className='display-1 mb-4'>{props.name}</h1>
+        <div className="subvisual text-center">
+            <h1 className='display-1'>{props.name}</h1>
             {
                 `${props.imgName}` === ''
                  ? ''
-                 : <img src={src} className="ratio mt-5 mb-4" /> 
+                 : <img src={src} className="ratio" /> 
             }
         </div>
         </>
