78f762f5daf37a480e0ed0e0a9f0e6f732b1e262
jungenjin <jej1217s1217@gmail.com>
2023-02-25 01:07:33 +0900

    jej main preview 반응형 수정

--- tm-front/src/pages/main.jsx
+++ tm-front/src/pages/main.jsx
@@ -19,31 +19,31 @@ function Main() {
                 <div className="container-sm">
                   <div class="row-cols-8">
                     <div class="col">
-                      <div className="Pimg1" style={{background: 'url(/resource/main/Preview1.png)' }}></div>
+                      <div className="Pimg1"><img src={process.env.PUBLIC_URL + '/resource/main/Preview1.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg2" style={{background: 'url(/resource/main/Preview2.png)'}}></div>
+                      <div className=" Pimg2"><img src={process.env.PUBLIC_URL + '/resource/main/Preview2.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg3" style={{background: 'url(/resource/main/Preview3.png)'}}></div>
+                      <div className=" Pimg3"><img src={process.env.PUBLIC_URL + '/resource/main/Preview3.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg4" style={{background: 'url(/resource/main/Preview4.png)'}}></div>
+                      <div className=" Pimg4"><img src={process.env.PUBLIC_URL + '/resource/main/Preview4.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg5" style={{background: 'url(/resource/main/Preview5.png)'}}></div>
+                      <div className=" Pimg5"><img src={process.env.PUBLIC_URL + '/resource/main/Preview5.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg6" style={{background: 'url(/resource/main/Preview6.png)'}}></div>
+                      <div className=" Pimg6"><img src={process.env.PUBLIC_URL + '/resource/main/Preview6.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg7" style={{background: 'url(/resource/main/Preview7.png)'}}></div>
+                      <div className=" Pimg7"><img src={process.env.PUBLIC_URL + '/resource/main/Preview7.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg8" style={{background: 'url(/resource/main/Preview8.png)'}}></div>
+                      <div className=" Pimg8"><img src={process.env.PUBLIC_URL + '/resource/main/Preview8.png'}  className="Pi"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg9" style={{background: 'url(/resource/main/Preview9.png)'}}></div>
+                      <div className=" Pimg9"><img src={process.env.PUBLIC_URL + '/resource/main/Preview9.png'}  className="Pi"/></div>
                     </div>
                   </div>
                 </div>
