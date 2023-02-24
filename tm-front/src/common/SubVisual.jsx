0ec43711b6f1019e22e6251ef02feffd63eb0453
bye0607 <bye0607@gmail.com>
2023-02-24 11:02:50 +0900

    kej, SubVisual 컴포넌트 이미지 경로 수정

--- tm-front/src/common/SubVisual.jsx
+++ tm-front/src/common/SubVisual.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../style/common.css';
 
 function SubVisual(props) {
-    const src = `resource/common/${props.imgName}_visual.png`;
+    const src = `/resource/common/${props.imgName}_visual.png`;
     return (
         <div className="subvisual text-center pt-5 mt-5 pb-5">
             <h1 className='display-1 pt-5 mt-4 mb-4'>{props.name}</h1>
