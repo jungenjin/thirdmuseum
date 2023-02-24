944d473f60b3f38f46b30925047e6f9b0fee969e
jungenjin <jej1217s1217@gmail.com>
2023-02-25 01:39:59 +0900

    jej main 수정 중

--- tm-front/src/pages/main.jsx
+++ tm-front/src/pages/main.jsx
@@ -6,7 +6,7 @@ function Main() {
       <section className="App-Main">
         <section className="main">
           <div className="container-xxl">
-            <p className="h4">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
+              <p className="h4">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
               radiant boby  come to life” - by Tamara Shopsin</p>
               <div className="mainText2">Color</div>
               <div className="mainText3">is life</div>
@@ -19,31 +19,31 @@ function Main() {
                 <div className="container-sm">
                   <div class="row-cols-8">
                     <div class="col">
-                      <div className="Pimg1"><img src={process.env.PUBLIC_URL + '/resource/main/Preview1.png'}  className="Pi"/></div>
+                      <div className="Pimg1"><img src={process.env.PUBLIC_URL + '/resource/main/Preview1.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg2"><img src={process.env.PUBLIC_URL + '/resource/main/Preview2.png'}  className="Pi"/></div>
+                      <div className=" Pimg2"><img src={process.env.PUBLIC_URL + '/resource/main/Preview2.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg3"><img src={process.env.PUBLIC_URL + '/resource/main/Preview3.png'}  className="Pi"/></div>
+                      <div className=" Pimg3"><img src={process.env.PUBLIC_URL + '/resource/main/Preview3.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg4"><img src={process.env.PUBLIC_URL + '/resource/main/Preview4.png'}  className="Pi"/></div>
+                      <div className=" Pimg4"><img src={process.env.PUBLIC_URL + '/resource/main/Preview4.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg5"><img src={process.env.PUBLIC_URL + '/resource/main/Preview5.png'}  className="Pi"/></div>
+                      <div className=" Pimg5"><img src={process.env.PUBLIC_URL + '/resource/main/Preview5.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg6"><img src={process.env.PUBLIC_URL + '/resource/main/Preview6.png'}  className="Pi"/></div>
+                      <div className=" Pimg6"><img src={process.env.PUBLIC_URL + '/resource/main/Preview6.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg7"><img src={process.env.PUBLIC_URL + '/resource/main/Preview7.png'}  className="Pi"/></div>
+                      <div className=" Pimg7"><img src={process.env.PUBLIC_URL + '/resource/main/Preview7.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg8"><img src={process.env.PUBLIC_URL + '/resource/main/Preview8.png'}  className="Pi"/></div>
+                      <div className=" Pimg8"><img src={process.env.PUBLIC_URL + '/resource/main/Preview8.png'}  className="Pimg"/></div>
                     </div>
                     <div class="col">
-                      <div className=" Pimg9"><img src={process.env.PUBLIC_URL + '/resource/main/Preview9.png'}  className="Pi"/></div>
+                      <div className=" Pimg9"><img src={process.env.PUBLIC_URL + '/resource/main/Preview9.png'}  className="Pimg"/></div>
                     </div>
                   </div>
                 </div>
