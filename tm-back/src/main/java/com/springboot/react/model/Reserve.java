6d711add46455a0b049d6a9b7fedc76a34a4aa7d
bye0607 <bye0607@gmail.com>
2023-02-27 21:40:40 +0900

    kej, jpa에서 오라클 시퀀스 만들도록 수정

--- tm-back/src/main/java/com/springboot/react/model/Reserve.java
+++ tm-back/src/main/java/com/springboot/react/model/Reserve.java
@@ -3,29 +3,42 @@ package com.springboot.react.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import lombok.Data;
 
 @Data
+@DynamicInsert //Insert시 Null인 필드를 제외하기위해 사용
+@DynamicUpdate //update시 Null인 필드를 제외하기위해 사용
 @Entity
 @Table(name = "tb_reserve")
 public class Reserve{
+	//글번호
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TB_RESERVE_SEQ")
+	@SequenceGenerator(sequenceName = "RES_NO_SEQ", allocationSize = 1, name = "TB_RESERVE_SEQ")
 	@Column(name = "res_no")
 	private int resNo;
 	
+	//예약자
 	@Column(name = "res_reg_id")
 	private String resRegId;
 	
+	//예약일
 	@Column(name = "res_date")
 	private String resDate;
 	
+	//예약시간
 	@Column(name = "res_time")
 	private String resTime;
 	
+	//인원수
 	@Column(name = "res_personer")
 	private String resPersoner;
 	
