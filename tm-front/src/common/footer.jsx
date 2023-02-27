e9db8216e63311234d7190bb4fa1028e528e1e54
bye0607 <bye0607@gmail.com>
2023-02-27 23:53:56 +0900

    kej, footer컴포넌트 퍼블리싱 수정

--- tm-front/src/common/footer.jsx
+++ tm-front/src/common/footer.jsx
@@ -1,18 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '../style/common.css';
-
 function Footer() {
   return (
-      <div className="Footer">
-        <footer className="App-Footer">
-          <footer className="container-xxl p-3">
-              <div className="FtextBox">
-                <p className="Ftext">오시는 길</p>
-                <p className="Ftext">이용약관</p>
-                <p className="Ftext">개인정보처리방침</p>
-              </div>
-              <p className="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
-          </footer>
+      <div className="footer">
+        <footer className="container-xxl">
+            <div>
+              <a href="/location" className="Ftext">오시는 길</a>
+              <a href="#!" className="Ftext">이용약관</a>
+              <a href="#!" className="Ftext">개인정보처리방침</a>
+            </div>
+            <p className="FSubtext">Copyright © 2023 세번째미술관 All right reserved.</p>
         </footer>
       </div>
   );
