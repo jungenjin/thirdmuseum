e3062990ef8d8eb8cdeb642add89ad260f13c036
bye0607 <bye0607@gmail.com>
2023-02-27 23:46:41 +0900

    kej, 과거전시 퍼블리싱 수정

--- tm-front/src/pages/past.jsx
+++ tm-front/src/pages/past.jsx
@@ -1,89 +1,70 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container, Row, Col} from 'react-bootstrap';
 import SubVisual from '../common/SubVisual';
-import '../style/past.css'
 
 function past(){
     return(
         <>
         <SubVisual name={'과거전시'} imgName={''} />
-        <div className='container-xxl'>
+        <Container fluid="xxl">
             <Row>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_1.png" alt="past1" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">창동레지던시 입주보고서 2022: <br/>
-                            김 홀 리 첸</div>
-                            <div className="pastText">2022-11-25 ~ 2022-12-04</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_1.png" alt="past1" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">창동레지던시 입주보고서 2022: 김 홀 리 첸</p>
+                        <p>2022-11-25 ~ 2022-12-04</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_2.png" alt="past2" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">국립현대미술관 «2022 고양레지던시 오픈스튜디오 18 모두 다른 빛깔»</div>
-                            <div className="pastText">2022-11-11 ~ 2022-11-13</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_2.png" alt="past2" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">국립현대미술관 «2022 고양레지던시 오픈스튜디오 18 모두 다른 빛깔»</p>
+                        <p>2022-11-11 ~ 2022-11-13</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_3.png" alt="past3" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">김순기: 게으른 구름</div>
-                            <div className="pastText">2022-11-25 ~ 2022-12-04</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_3.png" alt="past3" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">김순기: 게으른 구름</p>
+                        <p>2022-11-25 ~ 2022-12-04</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_4.png" alt="past4" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">MMCA 과천프로젝트 2021: <br/>예술버스쉼터_쓸모없는 건축과<br/> 유용한 조각에 대하여</div>
-                            <div className="pastText">2021-12-15 ~ 2022-11-20</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_4.png" alt="past4" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">MMCA 과천프로젝트 2021: 예술버스쉼터_쓸모없는 건축과 유용한 조각에 대하여</p>
+                        <p>2021-12-15 ~ 2022-11-20</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_5.png" alt="past5" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">대지의 시간</div>
-                            <div className="pastText">2021-11-25 ~ 2022-03-27</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_5.png" alt="past5" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">대지의 시간</p>
+                        <p>2021-11-25 ~ 2022-03-27</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_6.png" alt="past6" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">DMZ 극장</div>
-                            <div className="pastText">2021-08-20 ~ 2021-10-03</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_6.png" alt="past6" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">DMZ 극장</p>
+                        <p>2021-08-20 ~ 2021-10-03</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_7.png" alt="past7" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">최욱경, 앨리스의 고양이</div>
-                            <div className="pastText">2021-10-27 ~ 2022-02-13</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_7.png" alt="past7" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">최욱경, 앨리스의 고양이</p>
+                        <p>2021-10-27 ~ 2022-02-13</p>
                     </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={3}>
-                    <div className="pastArt">
-                        <img src="../resource/display/past_8.png" alt="past8" className="pastImg"/>
-                        <div className="pastInfo">
-                            <div className="pastTitle">DMZ 극장</div>
-                            <div className="pastText">2021-08-20 ~ 2021-10-03</div>
-                        </div>
+                <Col xs={12} sm={6} md={4} lg={3} className="px-3 pb-5">
+                    <img src="/resource/display/past_8.png" alt="past8" className="ratio"/>
+                    <div className="py-4">
+                        <p className="fs-5 fw-bold text-wrap">DMZ 극장</p>
+                        <p>2021-08-20 ~ 2021-10-03</p>
                     </div>
                 </Col>
             </Row>
-        </div>
+        </Container>
         </>
         );
     }
