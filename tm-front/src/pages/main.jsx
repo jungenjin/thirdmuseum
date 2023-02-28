77fd297cccee37d2a449023e6b0a75444b72e963
jungenjin <jej1217s1217@gmail.com>
2023-02-28 10:34:46 +0900

    jej main 수정 중

--- tm-front/src/pages/main.jsx
+++ tm-front/src/pages/main.jsx
@@ -1,22 +1,30 @@
-import '../style/main.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import '../style/main.css';
+
 
 function Main() {
   return (
     <div className="Main">
       <section className="App-Main">
-        <section className="main mb-5">
-          <div className="container-xxl">
-              <p className="h4">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
-              radiant boby  come to life” - by Tamara Shopsin</p>
-              <div className="mainText2">Color</div>
-              <div className="mainText3">is life</div>
-              <button className="mainBtn" ><a className="mainTextBtn display-6" href='/now'>전시보러가기</a></button>
-          </div>
+        <section className="main">
+        <Container fluid="xxl">
+            <Row>
+              <Col md={1}></Col>
+                <Col xs={12} md={10}>
+                  <p className="h4">“I Think of Olimpia Zagnoli as Keith Haring’s<br/>
+                  radiant boby  come to life” - by Tamara Shopsin</p>
+                  <div className="mainText2">Color</div>
+                  <div className="mainText3">is life</div>
+                  <button className="mainBtn" ><a className="mainTextBtn display-6" href='/now'>전시보러가기</a></button>
+                </Col>
+              <Col md={1}></Col>
+            </Row>
+          </Container>
         </section>
         <section className='pt-5 content'>
-            <div className="container-xxl">
+          <Container fluid="xxl">
                 <h2 className="display-1 Ptext">Preview</h2>
                 <Row>
                   <Col md={1}></Col>
@@ -36,7 +44,7 @@ function Main() {
                     </Col>
                     <Col md={1}></Col>
                 </Row>
-                </div>
+              </Container>
             </section>
           </section>
       </div>
