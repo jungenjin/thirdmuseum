252a2a21e662b6f1b3e36822a45412297d791a08
answjd120 <answjd120@gmail.com>
2023-03-01 19:08:06 +0900

    kmj 로그인 페이지 css 수정 완료

--- tm-front/src/pages/login.jsx
+++ tm-front/src/pages/login.jsx
@@ -8,7 +8,7 @@ import "../style/login.css"; // 이 페이지의 커스텀 디자인을 위한 C
 const Login = () => {
   const history = useHistory(); // 라우팅을 위한 히스토리 객체
   const [formData, setFormData] = useState(null);
-  const [checked, setChecked] = useState(false);
+  const [checked] = useState(false);
   const handleInputChange = (e) => {
     // 아이디와 비밀번호가 실시간으로 바뀔때마다, state에 값이 업데이트 됩니다.
     setFormData({
@@ -20,11 +20,9 @@ const Login = () => {
   useEffect(() => {
     if (localStorage.getItem("login") === "true") {
       alert("이미 로그인 상태입니다.");
-      
       history.push("/main");
     }
   }, []);
-
 
   const handleSubmit = () => {
     // 로그인 버튼을 눌렀을때 동작합니다.
@@ -77,7 +75,7 @@ const Login = () => {
         <div className="column">
           <input
             outlined
-            className="userId inputlogin"
+            className="userId inputlogin ms-1"
             name="userId"
             id="id"
             placeholder="아이디를 입력해주세요."
@@ -88,7 +86,7 @@ const Login = () => {
         <div className="column">
           <input
             outlined
-            className="userPw inputlogin"
+            className="userPw inputlogin ms-1"
             name="userPw"
             type="password"
             placeholder="비밀번호를 입력해주세요."
@@ -98,7 +96,7 @@ const Login = () => {
 
         <br/>
         {/* <Link to="/main"> */}
-          <button className="btnlogin" label="로그인" raised onClick={handleSubmit}>로그인</button>
+          <button className="btnlogin ms-1 fs-2" label="로그인" raised onClick={handleSubmit}>로그인</button>
         {/* </Link> */}
         <hr/>
         <div className="column">
@@ -107,7 +105,7 @@ const Login = () => {
             callbackUrl="http://192.168.35.115:3000/management-callback"
             render={(props) => 
             <div onClick={props.onClick}>
-              <button className="btnnaverlogin" type="submit"><b className="Nv">N </b> 네이버 로그인</button>
+              <button className="btnnaverlogin ms-1 fs-2" type="submit"><b className="Nv">N </b> 네이버 로그인</button>
             </div>}
             onSuccess={(res) => responseLogin(res, "naver")}
             onFailure={() => console.log("naver login fail")}
